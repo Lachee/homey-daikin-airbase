515ca9eab7c8d2578927f36e4a4a3020af310f7d
Lachee <lake.hopkins@outlook.com>
2026-03-22 18:41:56 +1100

    basic flow control

--- app.ts
+++ app.ts
@@ -1,6 +1,13 @@
 'use strict';
 
-import Homey from 'homey';
+import Homey, {Device} from 'homey';
+import {DaikinClient} from "daikin-airbase";
+
+type ZoneControlArgs = {
+  device: Device;
+  zone_name: string;
+  enabled: boolean;
+};
 
 module.exports = class MyApp extends Homey.App {
 
@@ -8,7 +15,18 @@ module.exports = class MyApp extends Homey.App {
    * onInit is called when the app is initialized.
    */
   async onInit() {
-    this.log('MyApp has been initialized');
+    this.log('Airbase has been initialized');
+
+    const zoneControlAction = this.homey.flow.getActionCard('zone_control');
+    zoneControlAction.registerRunListener(async ({ device, zone_name, enabled } : ZoneControlArgs) => {
+      if (!('client' in device) || !device.client)
+        throw new Error(
+          'Device does not have a client, cannot control zone'
+        )
+
+      const client = device.client as DaikinClient;
+      await client.zones.setZone(zone_name, enabled);
+    })
   }
 
 }
